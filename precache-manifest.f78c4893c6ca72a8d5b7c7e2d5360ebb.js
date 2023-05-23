7c7fb6df1261673cacb97b67f843f514d8498c2b
Vusumuz99 <mpanza.vusi5@gmail.com>
2023-05-23 12:34:23 +0200

    Updates

---
+++ precache-manifest.f78c4893c6ca72a8d5b7c7e2d5360ebb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f979d1f0c0764da8b65d",
+    "url": "/vusi-cv/static/css/main.046185bf.chunk.css"
+  },
+  {
+    "revision": "f979d1f0c0764da8b65d",
+    "url": "/vusi-cv/static/js/main.f979d1f0.chunk.js"
+  },
+  {
+    "revision": "3c1a72282470ba5043ba",
+    "url": "/vusi-cv/static/js/runtime~main.3c1a7228.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/vusi-cv/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "bb09833fc1fefbf06860874f486ea852",
+    "url": "/vusi-cv/index.html"
+  }
+];
